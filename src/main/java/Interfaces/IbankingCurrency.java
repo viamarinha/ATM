25f4d1bb674c831e18dd4f16c71ec935bb8f96f9
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-11-02 22:01:11 -0500

    Revert "Revert "Initial commit""
    This reverts commit a1effa1d

---
+++ src/main/java/Interfaces/IbankingCurrency.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import AccountsTreatments.AtmStatus;
+
+public interface IbankingCurrency {
+
+
+    public void calculateTotalAmount();
+
+    public void withDrawcalculation(int amount);
+}
